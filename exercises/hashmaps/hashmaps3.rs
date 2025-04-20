352e4a9df17440b520fb022be119c5d5729a94fe
ZhaoCake <zhaocake@foxmail.com>
2025-04-20 21:15:28 +0800

    feat: finish some exercises.

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -1,20 +1,17 @@
 // hashmaps3.rs
 //
-// A list of scores (one per line) of a soccer match is given. Each line is of
-// the form : "<team_1_name>,<team_2_name>,<team_1_goals>,<team_2_goals>"
-// Example: England,France,4,2 (England scored 4 goals, France 2).
+// 给出了一个足球比赛的比分列表（每行一个）。每行的格式为：
+// "<队伍1名称>,<队伍2名称>,<队伍1进球>,<队伍2进球>"
+// 示例：England,France,4,2（英格兰进4球，法国进2球）。
 //
-// You have to build a scores table containing the name of the team, goals the
-// team scored, and goals the team conceded. One approach to build the scores
-// table is to use a Hashmap. The solution is partially written to use a
-// Hashmap, complete it to pass the test.
+// 你需要建立一个比分表，包含队伍名称、该队进球数和失球数。
+// 构建比分表的一种方法是使用哈希表（Hashmap）。解决方案已经部分
+// 使用哈希表实现，请完成它以通过测试。
 //
-// Make me pass the tests!
+// 让我通过测试！
 //
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
-
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -39,6 +36,21 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+        // Update team 1's score
+        let team_1 = scores.entry(team_1_name.clone()).or_insert(Team {
+            goals_scored: 0,
+            goals_conceded: 0,
+        }); // or_insert() will insert a new entry if the key does not exist
+        team_1.goals_scored += team_1_score; // 上面相当于初始化与获取，下面这两行是更新
+        team_1.goals_conceded += team_2_score;
+        // Update team 2's score
+        let team_2 = scores.entry(team_2_name.clone()).or_insert(Team {
+            goals_scored: 0,
+            goals_conceded: 0,
+        });
+        team_2.goals_scored += team_2_score;
+        team_2.goals_conceded += team_1_score;
     }
     scores
 }
